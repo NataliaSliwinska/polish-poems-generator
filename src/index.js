5c747fe29b79f5481040043e4f9071c2e32cfedd
NataliaSliwinska <natalia.k.sliwinska@gmail.com>
2026-08-12 12:48:48 +0100

    added basic form handling in js, typewriter plug-in

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings:
+      "Śpieszmy się kochać ludzi, tak szybko odchodzą, zostaną po nich buty i telefon głuchy",
+    autoStart: true,
+    delay: 4,
+    cursor: "",
+  });
+}
+let poemFormElement = document.querySelector("#poem-form");
+poemFormElement.addEventListener("submit", generatePoem);
